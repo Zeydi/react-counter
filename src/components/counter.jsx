66c0c0ccc20794e7d940f6ea890dff36383a95a5
Zeydi <oussema.zeidi@ieee.org>
2019-01-01 02:00:23 +0100

    added styles to counter component

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -7,10 +7,19 @@ class Counter extends Component {
   handleIncrement = () => {
     this.setState({ count: this.state.count + 1 });
   };
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? <h6>Zero</h6> : count;
+  }
   render() {
     return (
       <div>
-        <span>{this.state.count}</span>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
           className="btn btn-secondary btn-sm"
           onClick={this.handleIncrement}
